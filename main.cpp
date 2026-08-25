b3636eab0f26fabe25cbcf8f9ae1299f2952a3a4
hanenashraf610-ux <hanenashraf610@icloud.com>
2026-08-25 22:30:47 +0300

    project

--- main.cpp
+++ main.cpp
@@ -11,3 +11,6 @@ s.print();
 cout<<"enter student data"<<endl;
 cout<<"print student data"<<endl;
 }
+/*What are the adventages of using static members in oop?
+Memory efficiency : static variables are created only once and shared across all objects of the class ,saving memory.
+Direct access :static functions can be called directly using the class name without needing to create an object.*/
