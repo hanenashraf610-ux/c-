55734640e97b6c3eccf028fc6ac688120981696d
hanenashraf610-ux <hanenashraf610@icloud.com>
2026-08-25 20:19:30 +0300

    university

---
+++ Student.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#ifndef STUDENT_H
+#define STUDENT_H
+#include<Person.h>
+using namespace std;
+class Student:public Person
+{
+    private:
+    string university
+    public:
+    Student()
+    {
+university="no university";
+    }
+    Student(string name ,int age ,string university):Person(name,age)
+    {
+this->university=university;
+    }
+    void setUniversity(string university)
+    {
+        this->university=university;
+    }
+    string getUniversity()
+    {
+        return university;
+    }
+    void informations()
+    { 
+        Person::informations();
+        cout<<"enter your university"<<endl;
+        cin>>university;
+    }
+    void print()
+    {
+        Person::print();
+        cout<<"you university is"<<university;
+
+    }
+}#endif//STUDENT_H
--- task1.cpp
+++ task1.cpp
@@ -1,1 +1,13 @@
-////
+#include<iostream>
+using namespace std;
+#include<Person.h>
+#include<Student.h>
+
+int main()
+{
+Student s;
+s.informations();
+s.print();
+cout<<"enter student data"<<endl;
+cout<<"print student data"<<endl;
+}
